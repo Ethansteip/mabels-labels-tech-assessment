ddac53df0b4abcc3a81e36e9cdd601c11c355e23
Ethan <ethan.steip@gmail.com>
2023-03-29 16:25:32 -0400

    initial commit - added express, body-parser, morgan, nodeman and setup server

---
+++ server/app.js
@@ -0,0 +1,24 @@
+// declarations
+require('dotenv').config();
+const {ENVIROMENT, PORT} = process.env;
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+// import routes
+const catRoutes = require('./routes/todoRoutes');
+
+const app = express();
+
+// Middlewear setup
+app.use(morgan(ENVIROMENT));
+app.use(bodyParser.json());
+
+app.use('/cats', catRoutes);
+
+
+app.get('/', (req, res) => {
+  res.json({greetings: 'Welcome to Ethans To-do application'});
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port: http://localhost${PORT}`));
