49667b1f6276eb192ac04d8df226a335504b66f1
Ethan <ethan.steip@gmail.com>
2023-03-29 19:23:39 -0400

    setup data connection, created schema for todos, seed file and setup a db reset script that you can run in the terminal to re-seed the db.

---
+++ server/db/schema/01_create_todos.sql
@@ -0,0 +1,10 @@
+-- schema/01_create_todos.sql
+DROP TABLE IF EXISTS todos CASCADE;
+
+-- CREATE TODOS
+CREATE TABLE todos (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  comment VARCHAR(255),
+  status VARCHAR(255) NOT NULL
+);
