aed0dde674442753c97f407a3e4d4a160d1d1863
Ethan <ethan.steip@gmail.com>
2023-03-30 19:27:10 -0400

    added route validation for /todo/{id} and /todo routes

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const todos = require('../db/queries/todos');
 const auth = require('../middlewear/basicAuth');
+const ErrorMessage = require('../errors/ErrorMessage.js');
 
 // Gets a list of all To-Dos
 router.get('/todos', (req, res) => {
@@ -13,7 +14,29 @@ router.get('/todos', (req, res) => {
 
 // Create a new To-Do item
 router.post('/todo', auth.isAuthorized, (req, res) => {
-  const {name, comment , status} = req.body;
+  let {name, comment, status} = req.body;
+  console.log("Status: ", status);
+
+  // Verifies that at least the name value is provided. Status will default to "todo" and comments can be null.
+  if (!name) {
+    res.status(400);
+    const error = new ErrorMessage(400);
+    res.json(error);
+    return;
+  }
+
+  if (status === undefined) {
+    status = 'todo';
+  }
+  // Allow only acceptable status values.
+  const statusArray = ['todo', 'inprogress', 'complete'];
+
+  if (!statusArray.includes(status)) {
+    const error = new ErrorMessage(400);
+    res.json(error);
+    return;
+  }
+
   todos.createNewTodo(name, comment, status).then(data => {
     res.setHeader("Content-Type", "application/json");
     res.status(201);
@@ -21,14 +44,30 @@ router.post('/todo', auth.isAuthorized, (req, res) => {
   });
 });
 
-// Returns an object of a specific todo based on the id.
+// Get a specific To-Do
 router.get('/todo/:id', (req, res) => {
-  // validate the parameter, return 400 if not an int, then continue
-  // if id canno't be found, return a 404.
-  todos.getTodoById(req.params.id).then(data => {
-    console.log(data);
+
+  const id = req.params.id;
+  console.log("id type: ", typeof parseInt(id));
+
+
+
+  todos.getTodoById(id).then(data => {
+    if (data.length === 0) {
+      res.status(404);
+      const error = new ErrorMessage(404);
+      res.json(error);
+      return;
+    }
+    res.status(200);
     res.json(data[0]);
   });
+});
+
+router.get('/todo', (req, res) => {
+  const error = new ErrorMessage(404);
+  res.json(error);
+  return;
 });
 
 module.exports = router;
